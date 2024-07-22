ac1712f592cdfa532319b667ed73d7d4ddaf3f65
Martin Skachkov <mgsemailbox@gmail.com>
2024-07-22 18:19:14 +0300

    add logo img and cancel edit button

--- 1. Python/Project 1/wiki/encyclopedia/urls.py
+++ 1. Python/Project 1/wiki/encyclopedia/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"), # The angle brackets are used to capture part of the URL as a variable that can be passed to the view function.
     path("search/", views.search, name="search"),
     path("new/", views.new_page, name="new_page"),
     path("edit/", views.edit, name="edit"),
