3612a210d5b821c4b56cbb213bcd0a5f0bd54af7
Martin Skachkov <mgsemailbox@gmail.com>
2024-07-23 18:36:09 +0300

    add frontend for job application app

---
+++ 1. Python/My_Projects/Project_1/job_application/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create database table.
+class Form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=80)
+
+    # __str__ method in a class is a special method that is used to define the "informal" or
+    # nicely printable string representation of an instance of that class (p=Person() print(p))
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
