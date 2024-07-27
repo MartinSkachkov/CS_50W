f8fbfeec761d00b7f5c9f6efcd578685092da221
Martin Skachkov <mgsemailbox@gmail.com>
2024-07-27 13:15:17 +0300

    add duplicating resume names fix

--- 1. Python/My_Projects/Project_1/job_application/models.py
+++ 1. Python/My_Projects/Project_1/job_application/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+import uuid
+
+def unique_upload_to(instance, filename):
+    # Keep the directory structure and add a unique identifier
+    upload_directory = 'resumes/pdfs/'
+    ext = filename.split('.')[-1]
+    unique_filename = f"{uuid.uuid4().hex}.{ext}"
+    return f'{upload_directory}{unique_filename}'
 
 # Create database table called Form.
 class Form(models.Model):
@@ -7,7 +15,7 @@ class Form(models.Model):
     email = models.EmailField()
     date = models.DateField()
     occupation = models.CharField(max_length=80)
-    resume = models.FileField(upload_to='resumes/pdfs/')
+    resume = models.FileField(upload_to=unique_upload_to)
 
     # __str__ method in a class is a special method that is used to define the "informal" or
     # nicely printable string representation of an instance of that class (p=Person() print(p))
